a272f7d31f0f0a93068ef1543d230f5ccc88c1e4
laAna <analydia.ma@hotmail.com>
2017-11-30 22:35:40 -0600

    update all

--- API/src/model/db_usuarios.js
+++ API/src/model/db_usuarios.js
@@ -1,60 +1,31 @@
 var usuario = require('./object_definitions').usuario;
 var cloudant= require('../cloudant/cloudant_connection').cloudant;
 var indexes = require('../cloudant/cloudant_indexes').object_indexes;
-var db = cloudant.db.use(process.env.CLOUDANT_SERVICE_NAME);
+var passchecker = require("./passcheck");
+var db = cloudant.db.use('users');
 
-var functions = function(usuario, db_name, indexes) {
-  var get = function(id) {
-    return new Promise(function(resolve, reject) {
-      if (id) {
-        db.get(id, function(err, data) {
-          if (data) {
-            resolve(data);
-          } else {
-            reject(204);
-          }
-        });
-      } else {
-        reject(400);
-      }
-    });
-  }
+var functions = (function() {
   var list = function() {
-    return new Promise(function(resolve, reject) {
-      db.list({
-        include_docs: true
-      }, function(err, data) {
+    return new Promise(function(fulfill, reject) {
+      db.list({include_docs: true}, function(err, data) {
         if (data) {
-          var result = [];
-          data.rows.forEach(function(row) {
-
-            if (row.id.substr(0, 8) !== '_design/') {
-              result.push(row.doc);
-            }
-          });
-          resolve(result);
+          fulfill(result);
         } else {
-          reject(204);
+          reject(404);
         }
       });
     });
   }
   var create = function(new_object) {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function(fulfill, reject) {
       if(new_object != undefined){
-        if(new_object._rev != undefined){
-          delete new_object._rev;
+        if(new_object.username != undefined && new_object.password != undefined){
+          new_object.password = passchecker.salt_and_hash(new_object.password);
+          new_object._id = new_object.username;
         }
-        var model = JSON.parse(JSON.stringify(usuario));
-        Object.keys(new_object).forEach(function(key){
-
-          if(model[key] !== undefined){
-            model[key] = new_object[key];
-          }
-        });
-        db.insert(model, function(err, data){
+        db.insert(new_object, function(err, data){
           if(data){
-            resolve(data);
+            fulfill(data);
           }else{
             reject(500);
           }
@@ -64,45 +35,14 @@ var functions = function(usuario, db_name, indexes) {
       }
     });
   }
-  var update = function(new_object) {
-    return new Promise(function(resolve, reject) {
-      if(new_object != undefined && new_object._id != undefined){
-        if(new_object._rev != undefined){
-          delete new_object._rev;
-        }
-        db.get(new_object._id,function(err, old_object){
-          if(old_object){
-            Object.keys(new_object).forEach(function(key){
-              if(old_object[key] !== undefined){
-                old_object[key] = new_object[key];
-              }
-            });
-            db.insert(old_object, function(err, response){
-              if(response){
-                old_object._rev = response.rev;
-                resolve(old_object);
-              }else{
-                reject(500);
-              }
-            });
-          }else{
-            reject(204);
-          }
-        });
-      }else{
-        reject(400);
-      }
-    });
-  }
-  var destroy = function(id) {
-    return new Promise(function(resolve, reject) {
+  var destroy = function(username) {
+    return new Promise(function(fulfill, reject) {
       if(id){
-        db.get(id, function(err, data){
+        db.get(username, function(err, data){
           if(data){
             db.destroy(data._id,data._rev,function(err, data){
-
               if(data){
-                resolve(data);
+                fulfill(data);
               }else{
                 reject(500);
               }
@@ -116,78 +56,33 @@ var functions = function(usuario, db_name, indexes) {
       }
     });
   }
-
-  var search = function(query) {
-    return new Promise(function(resolve, reject) {
-      db.find(query, function(err, result) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(result.docs);
-        }
-      });
-    });
-  }
-
-  var resetDB = function() {
-    return new Promise(function(resolve, reject) {
-      cloudant.db.destroy(process.env.CLOUDANT_SERVICE_NAME, function(err) {
-        if (err) {
-          console.log(err);
-        } else {
-          cloudant.db.create(process.env.CLOUDANT_SERVICE_NAME, function() {
-            if (err) {
-              console.log(err);
-            } else {
-              db = cloudant.db.use(process.env.CLOUDANT_SERVICE_NAME);
-              initDB().then(function(response){
-                resolve(response)
-              }).catch(function(err){
-                reject(err);
-              });
-            }
-          })
-        }
-      })
-    })
-  }
-
-  var initDB = function() {
-    return new Promise(function(resolve, reject) {
-      if(!indexes){
-        resolve(200);
+  var login = function(username, password){
+    return new Promise(function(fulfill,reject){
+      if(!username || !password){
+        reject(400);
       }else{
-        var length = Object.keys(indexes).length;
-        var count = 0;
-        Object.keys(indexes).forEach(function(key) {
-          db.index(indexes[key], function(err, response) {
-            ++count;
-            if (err) {
-              reject(err);
-              console.log(err);
-            } else {
-              console.log('"' + indexes[key].name + '" index created');
+        db.get(username, function(err, data){
+          if(err){
+            reject(403);
+          }else{
+            if(passchecker.passwords_match(data.password,password)){
+              delete data.password;
+              fulfill(data);
+            }else{
+              reject(403);
             }
-            if (count == length) {
-              resolve(200);
-            }
-          });
+          }
         });
       }
     })
-  }
+  };
 
   return {
-    'db': db,
-    'get': get,
     'list': list,
     'create': create,
-    'update': update,
     'destroy': destroy,
-    'resetDB': resetDB,
-    'initDB': initDB,
-    'search': search
-  }
-}
+    'login': login
+  };
 
-module.exports.functions = functions
+})();
+module.exports = functions;
--- API/src/routes/usuarios_router.js
+++ API/src/routes/usuarios_router.js
@@ -1,50 +1,41 @@
 var express = require('express');
 var db = require('../model/db_usuarios');
-var guides_router = express.Router();
-
+var usuarios_router = express.Router();
 var auth = require('../auth/auth.js');
 var cloudant_queries = require('../cloudant/cloudant_queries');
 
-guides_router.route('/')
+usuarios_router.route('/')
   .get(function(req, res) {
-    if (process.env.NODE_ENV == 'test' || auth.verifyToken(req.headers.token)) {
-      db.list().then(function(data) {
-        res.status(200).send(data);
-      })
-    } else {
-      res.status(403).send({});
-    }
+    db.list().then(function(data) {
+      res.status(200).send(data);
+    }).catch(function(err) {
+      res.status(404).send({});
+    })
   })
   .post(function(req, res) {
-    if (process.env.NODE_ENV == 'test' || auth.verifyToken(req.headers.token)) {
-      db.create(req.body).then(function(data) {
-        res.status(201).send(data);
-      }).catch(function(err) {
-        res.status(400).send({});
-      })
-    } else {
-      res.status(403).send({});
-    }
-  })
-  .put(function(req, res) {
-    if (process.env.NODE_ENV == 'test' || auth.verifyToken(req.headers.token)) {
-      db.update(req.body).then(function(data) {
-        res.status(200).send(data);
-      })
-    } else {
-      res.status(403).send({});
-    }
+    db.create(req.body).then(function(data) {
+      res.status(201).send(data);
+    }).catch(function(err) {
+      res.status(400).send({});
+    })
   })
 
-guides_router.route('/:id')
-  .get(function(req, res) {
-    if (process.env.NODE_ENV == 'test' || auth.verifyToken(req.headers.token)) {
-      db.get(req.params.id).then(function(data) {
-        res.status(200).send(data);
-      })
-    } else {
-      res.status(403).send({});
-    }
+usuarios_router.route('/:id')
+  .delete(function(req, res) {
+    db.destroy(req.params.id).then(function(data) {
+      res.status(200).send(data);
+    }).catch(function(err) {
+      res.status(400).send({});
+    })
   })
 
-module.exports = guides_router;
+usuarios_router.route('/auth')
+  .post(function(req, res) {
+    db.login(req.body).then(function(data) {
+      res.status(200).send(data);
+    }).catch(function(err) {
+      res.status(403).send({});
+    })
+  })
+
+module.exports = usuarios_router;
