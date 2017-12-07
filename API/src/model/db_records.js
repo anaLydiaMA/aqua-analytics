6fc949df089956a365ce98c6fdace5bee9c01daa
laAna <analydia.ma@hotmail.com>
2017-12-06 18:43:11 -0600

    switch routes categories

--- API/src/model/db_records.js
+++ API/src/model/db_records.js
@@ -3,11 +3,11 @@ var indexes = require('../cloudant/cloudant_indexes').object_indexes;
 var db = cloudant.db.use('data');
 
 var functions = (function() {
-  var get = function() {
+  var get = function(doc) {
     return new Promise(function(fulfill, reject) {
-      db.get('GRAPHS', function(err, data) {
+      db.get(doc, function(err, data) {
         if (data) {
-          fulfill(data.data);
+          fulfill(data.graph);
         } else {
           reject(204);
         }
