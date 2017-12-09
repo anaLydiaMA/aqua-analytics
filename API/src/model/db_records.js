cdf83f0a618fbff3af57956ab4fb141cbdfd3a7b
laAna <analydia.ma@hotmail.com>
2017-12-08 19:05:04 -0600

    adding sort and increment value funcionality

--- API/src/model/db_records.js
+++ API/src/model/db_records.js
@@ -14,8 +14,44 @@ var functions = (function() {
       });
     });
   }
+
+  var rating = function(doc) {
+    return new Promise(function(fulfill, reject) {
+      db.get('GRAPHS', function(err, object) {
+        if (object) {
+          var municipios = ['Guadalajara','Zapopan','San Pedro Tlaquepaque','Tonalá','Tlajomulco de Zúñiga','El Salto','Ixtlahuacán de los Membrillos','Juanacatlán'];
+          var results = [
+            {'municipio': 'Guadalajara','value': 1},
+            {'municipio': 'Zapopan','value': 4},
+            {'municipio': 'San Pedro Tlaquepaque','value': 7},
+            {'municipio': 'Tonalá','value': 0},
+            {'municipio': 'Tlajomulco de Zúñiga','value': 3},
+            {'municipio': 'El Salto','value': 4},
+            {'municipio': 'Ixtlahuacán de los Membrillos','value': 5},
+            {'municipio': 'Juanacatlán','value': 2}
+          ];
+          for (i in object.data){
+            //all algotitm
+          }
+          var pos = 'Juanacatlán';
+          results[municipios.indexOf(pos)].value++;
+          results.sort(function(a, b){
+              return b.value - a.value;
+          });
+          var top = [];
+          top.push(results[0].municipio);
+          top.push(results[1].municipio);
+          top.push(results[2].municipio);
+          fulfill(top);
+        } else {
+          reject(204);
+        }
+      });
+    });
+  }
   return {
-    'get': get
+    'get': get,
+    'rating': rating
   };
 
 })();
